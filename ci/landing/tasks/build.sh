6e71e41f6d937b771593ae89560bab3ab3212122
Steve Greenberg <spgreenberg@gmail.com>
2020-02-09 09:52:54 -0700

    construct source URL

--- ci/landing/tasks/build.sh
+++ ci/landing/tasks/build.sh
@@ -4,14 +4,15 @@ set -e
 
 version=$(cat version/version)
 commit_hash=$(cat page-source/.git/ref)
+source_link="https://github.com/cloudfoundry-tutorials/tutorials/commit/${commit_hash}"
 
 echo "Building tutorial landing page version: ${version} commit hash: ${commit_hash}"
 
 pushd page-source/landing-page
 	sed -i "s/((version))/${version}/g" index.html
-	sed -i "s/((source))/${commit_hash}/g" index.html
+	sed -i "s/((source))/${source_link}/g" index.html
 
   artifact_name="landing-page-${version}.zip"
-	zip -r ${artifact_name} * -x "*.DS_Store" -x "manifest.yml"
+	zip -r ${artifact_name} * -x "*.DS_Store" -x "*-manifest.yml"
 	mv ${artifact_name} ../../site
 popd

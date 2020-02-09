5c683bcd38b659cd3f1c8194329269b7b854da25
Steve Greenberg <spgreenberg@gmail.com>
2020-02-09 10:41:55 -0700

    inject copyright year from bash

--- ci/landing/tasks/build.sh
+++ ci/landing/tasks/build.sh
@@ -4,12 +4,14 @@ set -e
 
 version=$(cat version/version)
 commit_hash=$(cat page-source/.git/ref)
+year=$(date +%Y)
 
 echo "Building tutorial landing page version: ${version} commit hash: ${commit_hash}"
 
 pushd page-source/landing-page
 	sed -i "s/((version))/${version}/g" index.html
 	sed -i "s/((commit))/${commit_hash}/g" index.html
+	sed -i "s/((year))/${year}/g" index.html
 
   artifact_name="landing-page-${version}.zip"
 	zip -r ${artifact_name} * -x "*.DS_Store" -x "*-manifest.yml"

64767e18564e2bf8749bd25f263623e2761278b7
Steve Greenberg <spgreenberg@gmail.com>
2020-02-09 10:34:09 -0700

    avoid full url escaping issues

--- ci/landing/tasks/build.sh
+++ ci/landing/tasks/build.sh
@@ -4,13 +4,12 @@ set -e
 
 version=$(cat version/version)
 commit_hash=$(cat page-source/.git/ref)
-source_link="https://github.com/cloudfoundry-tutorials/tutorials/commit/${commit_hash}"
 
 echo "Building tutorial landing page version: ${version} commit hash: ${commit_hash}"
 
 pushd page-source/landing-page
 	sed -i "s/((version))/${version}/g" index.html
-	sed -i "s/((source))/${source_link}/g" index.html
+	sed -i "s/((commit))/${commit_hash}/g" index.html
 
   artifact_name="landing-page-${version}.zip"
 	zip -r ${artifact_name} * -x "*.DS_Store" -x "*-manifest.yml"

98c4f39a2eb7b7443008247030493147d5acb849
Steve Greenberg <spgreenberg@gmail.com>
2020-03-15 17:44:43 -0600

    wip -tutorial pipeline

--- ci/set-pipelines.sh
+++ ci/set-pipelines.sh
@@ -4,3 +4,5 @@ target=cff-tutorials
 this_directory=$(dirname "$0")
 
 fly -t $target sp -p landing-page -c ${this_directory}/landing/pipeline.yml --non-interactive
+
+fly -t $target sp -p what-is-cf -c ${this_directory}/tutorial/pipeline.yml -l ${this_directory}/tutorial/configs/what-is-cf.yml --non-interactive

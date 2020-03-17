0f3129123263390536908dfbd078da9eb449bd15
Steve Greenberg <spgreenberg@gmail.com>
2020-03-16 19:23:59 -0600

    trycf tutorial

--- ci/set-pipelines.sh
+++ ci/set-pipelines.sh
@@ -6,3 +6,5 @@ this_directory=$(dirname "$0")
 fly -t $target sp -p landing-page -c ${this_directory}/landing/pipeline.yml --non-interactive
 
 fly -t $target sp -p what-is-cf -c ${this_directory}/tutorial/pipeline.yml -l ${this_directory}/tutorial/configs/what-is-cf.yml --non-interactive
+
+fly -t $target sp -p trycf -c ${this_directory}/tutorial/pipeline.yml -l ${this_directory}/tutorial/configs/trycf.yml --non-interactive

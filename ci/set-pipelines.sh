903161281016760798bb340d600fb82a82d64cce
Steve Greenberg <spgreenberg@gmail.com>
2020-03-30 09:01:25 -0600

    add cf-and-k8s tutorial to ci

--- ci/set-pipelines.sh
+++ ci/set-pipelines.sh
@@ -8,3 +8,5 @@ fly -t $target sp -p landing-page -c ${this_directory}/landing/pipeline.yml --no
 fly -t $target sp -p what-is-cf -c ${this_directory}/tutorial/pipeline.yml -l ${this_directory}/tutorial/configs/what-is-cf.yml --non-interactive
 
 fly -t $target sp -p trycf -c ${this_directory}/tutorial/pipeline.yml -l ${this_directory}/tutorial/configs/trycf.yml --non-interactive
+
+fly -t $target sp -p cf-and-k8s -c ${this_directory}/tutorial/pipeline.yml -l ${this_directory}/tutorial/configs/cf-and-k8s.yml --non-interactive
